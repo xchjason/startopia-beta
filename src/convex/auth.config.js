5a2ce5ce33b4782fafbc571d9cf442ef26a799be
Jason Xiang <xiangchuhang@gmail.com>
2024-09-17 02:51:11 -0400

    auth_config

--- src/convex/auth.config.js
+++ src/convex/auth.config.js
@@ -1,8 +1,8 @@
 const authConfig = {
   providers: [
     {
-      domain: process.env.AUTH0_DOMAIN || 'dev-wbnbi7kco2rcxkqp.us.auth0.com',
-      applicationID: process.env.AUTH0_CLIENT_ID || 'TFyktmqjl4PGkxv3ZtEf4xPOb4dsGjj5',
+      domain: process.env.AUTH0_DOMAIN,
+      applicationID: process.env.AUTH0_CLIENT_ID,
     },
   ],
 };
